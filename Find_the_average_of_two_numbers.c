56f89fba7905938af7c48098aee4cc6a2fc53445
bapiraju1234 <112751566+bapiraju1234@users.noreply.github.com>
2022-10-12 22:37:44 +0530

    #include <stdio.h> int main() {     int num1, num2;     float avg;
        scanf("%d",&num1);
        scanf("%d",&num2);
    
        avg= (float)(num1+num2)/2;
    
        //%.2f is used for displaying output upto two decimal places
        printf("%.4f",avg);
    
        return 0;
    }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    float avg;
+
+    scanf("%d",&num1);
+    scanf("%d",&num2);
+
+    avg= (float)(num1+num2)/2;
+
+    //%.2f is used for displaying output upto two decimal places
+    printf("%.4f",avg);
+
+    return 0;
+}
